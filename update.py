6a31ae3fdad356cec7e9b3c91f4158f68b8beeb1
Monty Taylor <mordred@inaugust.com>
2013-08-04 14:15:53 -0400

    Bump pbr requirment to 0.5.21
    Removes the need for setuptools_git. Also, change the upper bound,
    as the next planned release will be 1.0 and we'd like to coordinate
    that move.
    
    Change-Id: I9a9ff8c62f800da3bf02d4afb3bc24ddfd0d24d5

--- update.py
+++ update.py
@@ -55,7 +55,7 @@ _setup_py_text = """#!/usr/bin/env python
 import setuptools
 
 setuptools.setup(
-    setup_requires=['pbr>=0.5.20'],
+    setup_requires=['pbr>=0.5.21,<1.0'],
     pbr=True)
 """
 
