9ee5d599a07c2907164bc6f4f61376494df2b21c
Joe Gordon <joe.gordon0@gmail.com>
2014-06-17 14:52:26 -0700

    Update skip pep8 clause to use hacking
    Now that projects don't directly use pep8 anymore but rather hacking,
    update the ignore pep8 clause to say hacking.
    
    Related patch to ignore hacking in the requirements sync check:
    I114b05fb1ec99ec1e0e0162988edb5fa5517df46
    
    Change-Id: Ib21646db18220b40db505856f4796c3d51228abc

--- update.py
+++ update.py
@@ -130,8 +130,8 @@ def _sync_requirements_file(source_reqs, dev_reqs, dest_path, suffix):
             old_pip = _parse_pip(old_require.lower())
 
             # Special cases:
-            # projects need to align pep8 version on their own time
-            if "pep8" in old_pip:
+            # projects need to align hacking version on their own time
+            if "hacking" in old_pip:
                 new_reqs.write(old_line)
                 continue
 
