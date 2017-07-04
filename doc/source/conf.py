41b9a317951cc76daa65e7937b3357b85e689cfa
Dirk Mueller <dirk@dmllr.de>
2017-07-04 09:06:43 +0200

    Correct latex_document settings for Sphinx 1.6.3
    Sphinx 1.6.3 is starting to validate the latex_document
    settings which caused documentation build to fail as our
    settings were invalid. With this change the validation passes
    on 1.6.3 which unblocks the gate.
    
    Change-Id: I1fb81ea1d7de561185a6ae31dd4bc73e1ac8f9ae

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -175,7 +175,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index',),
+    ('index', 'RequirementsGuide.tex', 'Global Requirements Guide',
+     u'OpenStack contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
