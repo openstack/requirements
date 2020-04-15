d0b389d840c75216ab2cc10df849cb98990b1503
Matthew Thode <mthode@mthode.org>
2020-04-15 13:46:54 -0500

    update requirements and constraints for py3 only Victoria
    generate-constraints -b blacklist.txt -p /usr/bin/python3.6 -p /usr/bin/python3.8 -r global-requirements.txt
    
    Change-Id: I2d58d1b524764ea1d46d6ab3a382783f6a002b34
    Signed-off-by: Matthew Thode <mthode@mthode.org>

--- openstack_requirements/cmds/update.py
+++ openstack_requirements/cmds/update.py
@@ -54,14 +54,6 @@ _setup_py_text = """# Copyright (c) 2013 Hewlett-Packard Development Company, L.
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
 setuptools.setup(
     setup_requires=['pbr>=2.0.0'],
     pbr=True)
